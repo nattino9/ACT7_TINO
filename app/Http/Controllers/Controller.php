d421119c5185865e3c9ed09d7b64f4398ad2a066
nattino9 <natino@my.cspc.edu.ph>
2024-04-17 12:19:16 -0800

    act7_tino

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,8 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function index(){
+        $products = "Product list form in ProductController";
+        return view('products.index', ['products' =>$products]);
+    }
 }
